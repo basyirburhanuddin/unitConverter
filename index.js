bddaf1c92381bba164f3b99fe667568330f9d006
Basyir <ambasydin@gmail.com>
2022-07-05 17:54:26 +0800

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /*
 1 meter = 3.281 feet
 1 liter = 0.264 gallon
-1 kilogram = 2.204 pound
+1 kilogram =  2.204 pound
 */
 
 let getUserInput=document.getElementById("getUserInput-el")
@@ -20,8 +20,7 @@ convertBtn.addEventListener("click",function()
     printVolume(getInput)
     printMass(getInput)  
     
-    // console.log(getInput)
-    
+    // console.log(getInput)    
 })
 
 //when user click back on the input field, it clears out
